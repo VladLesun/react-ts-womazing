a8fcda0df91f22c2d03de2b143ea9c733203f21c
Vlad Lesun <vladlesunmma@gmail.com>
2025-12-30 00:19:39 +0300

    path

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,7 +1,7 @@
 export { default as Button } from './Button/index';
 export { default as Input } from './Input/index';
 export { default as Logo } from './Logo/index';
-export { default as Preloader } from './Preloader/index';
+export { default as Preloader } from './Preloader/Preloader';
 
 export { default as CartForm } from './Forms/CartForm/index';
 export { default as ContactsForm } from './Forms/ContactsForm/index';
