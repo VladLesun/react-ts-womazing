a2d59173e0b12cdb3502b8fa11d7c6eb0314d999
Vlad Lesun <vladlesunmma@gmail.com>
2025-12-30 15:56:16 +0300

    refactor: change vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	resolve: {
-		alias: {
-			'@components': path.resolve(__dirname, 'src/components'),
-			'@pages': path.resolve(__dirname, 'src/pages'),
-			'@redux': path.resolve(__dirname, 'src/redux'),
-			'@styles': path.resolve(__dirname, 'src/styles'),
-		},
-	},
 	build: {
 		rollupOptions: {
 			output: {
