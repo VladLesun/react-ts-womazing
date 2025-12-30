48276f6368506aa68b233279250b037107b4f720
Vlad Lesun <vladlesunmma@gmail.com>
2025-12-30 12:27:07 +0300

    refactor: change path in order.types.ts

--- src/redux/order/order.types.ts
+++ src/redux/order/order.types.ts
@@ -1,4 +1,4 @@
-import type { TOrder } from '../../components/ui/Forms/OrderForm';
+import type { TOrder } from '../../components/UI/Forms/OrderForm';
 import type { TStateStatus } from '../redux.types';
 
 export type TSendOrder = {
